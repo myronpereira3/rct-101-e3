bc1e3294b0dac9870c975363197fcd06fd42a266
myronpereira3 <myronpereira3@gmail.com>
2022-06-06 17:01:04 +0530

    added

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import Product from "./Product/Product";
+import { CartContext } from "../../context/CartContext";
 
 const Products = () => {
-  return <div>{/* Code here */}</div>;
+  const [data, setData] = useState([]);
+  const {cartCount, increment, decrement} = useContext(CartContext);
+
+  const getData = () => {
+    axios.get("http://localhost:8080/products")
+    .then((r)=>setData(r.data))
+  }
+
+  // const add
+
+  useEffect(()=>{
+    getData();
+  },[])
+
+  return <div >
+    {
+      data.map((e)=>(
+        <Product key={e.id} e={e} inc={increment} dec={decrement} />
+      ))
+    }
+    
+  </div>;
 };
 
 export default Products;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,11 +2,17 @@ import React, { useState, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 const Login = () => {
+
+  const { login } = useContext(AuthContext);
+
   return (
     <div>
+      Login
+      <form action="" style={{display:"flex",flexDirection:"column",maxWidth:"210px",margin:"auto",gap:"10px"}}>
       <input data-cy="login-email" />
       <input data-cy="login-password" />
-      <button data-cy="login-submit"></button>
+      <button data-cy="login-submit" onClick={login}>Login</button>
+      </form>
     </div>
   );
 };
